104c57a263147e46335aaa3fb9f8d40435fe44f8
Donabest <rabiuazeez3@gmail.com>
2026-03-28 20:25:01 +0100

    text-input color on dark mode changed

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -9,7 +9,7 @@ function Input({ placeholder }: placeholder) {
     <div className="relative flex items-center justify-center w-full">
       <input
         placeholder={placeholder}
-        className=" px-4 py-3 bg-white w-full outline-0 rounded-lg drop-shadow-gray-300 drop-shadow-2xl focus:scale-101 dark:drop-shadow-slate-800"
+        className=" px-4 py-3 bg-white w-full outline-0 rounded-lg drop-shadow-gray-300 drop-shadow-2xl focus:scale-101 dark:drop-shadow-slate-800 dark:text-black"
       />
       <button className="absolute right-3 bg-blue-700 rounded-lg cursor-pointer hover:bg-blue-300 active:scale-95">
         <img src={LogoSymbol} alt={LogoSymbol} className="invert h-8 w-8" />
