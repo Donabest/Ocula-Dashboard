21d25448699e6ae10aa5b6d7cd2f9e4576825c8e
Donabest <rabiuazeez3@gmail.com>
2026-05-16 05:34:03 +0100

    fix:gitignore

--- src/Features/Setting/NotificationSetting.tsx
+++ src/Features/Setting/NotificationSetting.tsx
@@ -1,4 +1,4 @@
-import { Switch } from "#components/ui/switch";
+import { Switch } from "#components/shacnUi/switch";
 import SettingHeader from "../../ui/SettingHeader";
 
 function NotificationSetting() {
--- src/ui/PasswordInput.tsx
+++ src/ui/PasswordInput.tsx
@@ -1,6 +1,6 @@
-import { Button } from "#components/ui/button";
-import { Input } from "#components/ui/input";
-import { Label } from "#components/ui/label";
+import { Button } from "#components/shacnUi/button";
+import { Input } from "#components/shacnUi/input";
+import { Label } from "#components/shacnUi/label";
 import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
 
